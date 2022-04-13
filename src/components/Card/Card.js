830335ae648ec49c852fd092ce04acd79e282cd1
Practicum AmitBG217 <amit217@yandex.com>
2022-04-13 12:32:22 +0300

    Feat+Style: Intro div of Card

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,11 +1,13 @@
 import React from "react";
 import CardCss from "./Card.module.css";
 import Image from "../Image/Image";
+import Intro from "../Intro/Intro";
 
 export default function Card() {
   return (
     <section className={CardCss.card}>
       <Image />
+      <Intro />
     </section>
   );
 }
