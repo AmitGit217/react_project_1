5eb315c62ebd9fc1ef1673c830b72c77432a31b4
Practicum AmitBG217 <amit217@yandex.com>
2022-04-14 09:45:22 +0300

    Feat+Style: Footer component

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -2,5 +2,9 @@ import React from "react";
 import FooterCSS from "./Footer.module.css";
 
 export default function Footer() {
-  return <footer className={FooterCSS.footer}></footer>;
+  return (
+    <footer className={FooterCSS.footer}>
+      <p>© Amit Bar-Gil</p>
+    </footer>
+  );
 }
